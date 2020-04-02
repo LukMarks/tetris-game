60603f62855f1424e4664ec0e89e1398cd5664e9
LukMarks <lucas.mmoreno@hotmail.com>
2020-04-02 20:47:57 -0300

    Added rotate function

--- source.cpp
+++ source.cpp
@@ -2,8 +2,59 @@
 
 using namespace std;
 
+wstring tetromino[7];
+
+int Rotate(int px, int py, int r)
+{
+     switch (r % 4)
+     {
+        case 0: return py*4+px; // rotate for 0 degrees
+        case 1: return 12 + py - (px*4); // rotate for 90 degrees
+        case 2: return 15 - (py*4)-px; // rotate for 180 degrees
+        case 3: return 3 - py + (px*4); // rotate for 270 degrees
+     }
+     return 0
+}
+
 int main()
 {
+    // create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+    
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"....");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L"....");
+
+
 
     return 0;
 }
